30c50b1f6178957c6d2c7ae3d40e59ae4ce721c1
mocsmiths <mocsmiths@gmail.com>
2017-09-14 18:59:13 +0800

    9.14完善信息

--- zerg/application/extra/wx.php
+++ zerg/application/extra/wx.php
@@ -8,7 +8,7 @@
 
 return [
   'app_id' => 'wx410bf90c3f9b5cc7',
-  'app_secret' => 'wx410bf90c3f9b5cc7',
+  'app_secret' => '1eb47d348edafbd9d9b049bd6bd62045',
   'login_url'=> "https://api.weixin.qq.com/sns/jscode2session?" .
       "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
   'access_token_url' => "https://api.weixin.qq.com/cgi-bin/token?" .
