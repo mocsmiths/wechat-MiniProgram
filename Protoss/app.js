226930215294430b90ba20b58ef0ab2b4c47ba66
mocsmiths <mocsmiths@github.com>
2017-09-27 05:34:38 +0800

    重构小程序商城结构

--- Protoss/app.js
+++
@@ -1,9 +0,0 @@
-
-import { Token } from 'utils/token.js';
-
-App({
-  onLaunch:function(){
-    var token = new Token();
-    token.verify();
-  }
-});
--- zerg/public/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by 七月.
- * Author: 七月
- * Date: 2017/4/12
- * Time: 5:02
- */
-
-phpinfo();
